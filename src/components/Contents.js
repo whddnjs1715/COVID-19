198c803c3afab1bb3bf14260dce1c70ea7b1124b
whddnjs1715 <whddnjs1715@example.com>
2021-09-19 20:30:40 +0900

    Contents 생성 및 CSS 작성 #3

---
+++ src/components/Contents.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contents = () => {
+    return (
+        <section>
+            <h2>국내 코로나 현황</h2>
+            <div className="contents"></div>
+      </section>
+    )
+}
+
+export default Contents
