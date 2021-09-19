7928a9ab5f93fee535d38e79a3ed2b6f68553405
whddnjs1715 <whddnjs1715@example.com>
2021-09-19 20:20:33 +0900

    Header 생성 #1

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <header className="header">
+            <h1>COVID-19</h1>
+            <select>
+                <option>국내</option>
+                <option>해외</option>
+            </select>
+        </header>
+    )
+}
+
+export default header
