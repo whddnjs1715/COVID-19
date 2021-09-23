25a32618eac1ba91b3bff5a0ac554451ec250cda
whddnjs1715 <whddnjs1715@example.com>
2021-09-23 23:41:02 +0900

    Doughnut 그래프 생성 #11

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { Bar, Line } from 'react-chartjs-2'
+import { Bar, Line, Doughnut } from 'react-chartjs-2'
 import axios from 'axios'
 
 const Contents = () => {
     const [confirmedData, setConfirmedData] = useState({})
-    const [quarantinedData, setquarantinedData] = useState({})
+    const [quarantinedData, setQuarantinedData] = useState({})
+    const [comparedData, setComparedData] = useState({})
 
     useEffect(() => {
         const fetchEvents = async() => {
@@ -38,6 +39,7 @@ const Contents = () => {
                     findItem.recovered = recovered;
                     findItem.confirmed = confirmed;
                 }
+                console.log("recovered : " + recovered);
                 return acc;
             }, [])
             
@@ -57,7 +59,7 @@ const Contents = () => {
                 ]
             });
 
-            setquarantinedData({
+            setQuarantinedData({
                 labels: labels, // 두개가 같으므로 labels만 적어도 사용가능
                 // 예 labels
                 datasets: [
@@ -69,10 +71,26 @@ const Contents = () => {
                     },
                 ]
             });
+
+            const last = arr[arr.length -1]
+
+            console.log(arr);
+            setComparedData({
+                labels: ["확진자", "격리해제", "사망"],
+                datasets: [
+                    {
+                        label: "누적 확진, 해제, 사망 비율",
+                        backgroundColor: ["#ff3d67", "#059bff", "#ffc233"],
+                        borderColor: ["#ff3d67", "#059bff", "#ffc233"],
+                        fill: false,
+                        data: [last.confirmed, last.recovered, last.death],
+                    },
+                ]
+            });
         }
 
         fetchEvents();
-    })
+    }, [])
 
     return (
         <section>
@@ -92,6 +110,15 @@ const Contents = () => {
                         data={quarantinedData}
                         options={
                             {title:{display: true, text: "월별 격리자 현황 추이", fontSize: 16}},
+                            {legend: {display: false, position: "bottom"}}
+                        }
+                    />
+                </div>
+                <div>
+                    <Doughnut
+                        data={comparedData}
+                        options={
+                            {title:{display: true, text: `누적 확진, 해제, 사망 (${new Date().getMonth+1}월)`, fontSize: 16}},
                             {legend: {display: false, position: "bottom"}}
                         }
                     />
